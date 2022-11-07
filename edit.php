5ef40ed6df11fecbc68e51ae9ea584bdad4a918f
DzakiNur <dzakinurmuhammadaflahsuherman@smkwikrama.sch.id>
2022-11-07 21:36:16 +0700

    tambah delete

--- edit.php
+++ edit.php
@@ -1,14 +1,13 @@
 <?php
-
-$servename = "localhost";
+$servername = "localhost";
 $username = "root";
 $password = "";
 $database = "student";
 
 //create connection
-$connection = new mysqli($servename, $username, $password, $database);
+$connection = new mysqli($servername, $username, $password, $database);
 
-
+$id = "";
 $name = "";
 $email = "";
 $phone = "";
@@ -17,41 +16,64 @@ $address = "";
 $errorMessage = "";
 $successMessage = "";
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    //GET method: Show the data of the student
+
+    if(!isset($_GET['id'])) {
+        header("location: /crud-student/index.php");
+        exit;
+    }
+
+    $id = $_GET['id'];
+
+    //read all row form database table
+    $sql="SELECT * FROM students WHERE id=$id";
+    $result=$connection->query($sql);
+    $row=$result->fetch_assoc();
+
+    if (!$row){
+        header("location: /crud-student/index,php");
+        exit;
+    }
+
+    $name = $row["name"];
+    $email = $row["email"];
+    $phone = $row["phone"];
+    $address = $row["address"];
+}else{
+    //POST method: update the data of the student
+
+    $id = $_POST["id"];
     $name = $_POST["name"];
     $email = $_POST["email"];
     $phone = $_POST["phone"];
     $address = $_POST["address"];
 
     do{
-        if(empty($name) || empty($email) || empty($phone) || empty($address)) {
+        if(empty($id) || empty($name) || empty($email) || empty($phone) || empty($address)) {
             $errorMessage = "All the fields are required";
             break;
         }
 
-        //menambah student ke database
-        $sql =  "INSERT INTO students (name, email, phone, address) " .  
-                "VALUES ('$name', '$email', '$phone', '$address')";
+        $sql =  "UPDATE students " . 
+                "SET name = '$name', email = '$email', phone = '$phone', address = '$address'" .
+                "WHERE id = $id";
+
         $result = $connection->query($sql);
 
         if(!$result){
-            $errorMessage = "Invalid query: " . $connection->error;
+            $errorMessage = "Invalid query " . $connection->error;
             break;
         }
 
-        $name = "";
-        $emai = "";
-        $phone = "";
-        $address = "";
+        $successMessage = "Student updated correctly";
 
-        $successMessage = "Client added correctly";
+        header("location: /crud-student/index.php");
+        exit;
 
-
-
-    }while(false);
+    }while(true);
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -78,6 +100,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         ?>
 
         <form method="post">
+            <input type="hidden" name="id" value="<?php echo $id; ?>">
             <div class="row mb-3">
                 <label class="col-sm-3 col-form-label">Name</label>
                 <div class="col-sm-6">
