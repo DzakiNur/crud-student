3dd496183e20783e4ec2420c9a1cbde3546399ec
DzakiNur <dzakinurmuhammadaflahsuherman@smkwikrama.sch.id>
2022-11-07 21:22:40 +0700

    tambah index

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shopping</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <title>List Students</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
 </head>
 <body>
     <div class="container my-5">
-        <h2>List of Clients</h2>
-        <a class="btn btn-primary" href="/mvc-myshop/create.php" role="button">New Client</a>
+        <h2>List of Students</h2>
+        <a class="btn btn-primary" href="/crud-student/create.php" role="button">New Student</a>
         <br>
         <table class="table">
             <thead>
@@ -29,7 +29,7 @@
                 $servername="localhost";
                 $username="root";
                 $password="";
-                $database="myshop";
+                $database="student";
 
                 //create connection
                 $connection = new mysqli($servername, $username, $password, $database);
@@ -40,7 +40,7 @@
                 }
 
                 //read all row form database table
-                $sql="SELECT * FROM clients";
+                $sql="SELECT * FROM students";
                 $result=$connection->query($sql);
 
                 if(!$result){
@@ -58,8 +58,8 @@
                     <td>$row[address]</td>
                     <td>$row[created_at]</td>
                     <td>
-                        <a class='btn btn-primary btn-sm' href='/mvc-myshop/edit.php?id=$row[id]'>Edit</a>
-                        <a class='btn btn-danger btn-sm' href='/mvc-myshop/delete.php?id=$row[id]'>Delete</a>
+                        <a class='btn btn-primary btn-sm' href='/crud-student/edit.php?id=$row[id]'>Edit</a>
+                        <a class='btn btn-danger btn-sm' href='/crud-student/delete.php?id=$row[id]'>Delete</a>
                     </td>
                 </tr>
                     ";
